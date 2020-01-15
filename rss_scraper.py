485f75ddaccd1b6761e78b52e0a6bba40e47bd76
Soroush Khanlou <soroushkhanlou@gmail.com>
2020-01-15 18:27:11 +1300

    updates to python script

--- rss_scraper.py
+++ rss_scraper.py
@@ -17,9 +17,13 @@ for f in os.listdir(root_dir + "/_posts"):
 
 d = feedparser.parse("./episodes.rss")
 
-def copy_value(rss_dict, lines, key):
+def copy_value(rss_dict, lines, key, new_key = None, as_string = False):
     if key in rss_dict:
-        lines.append('{}: {}\n'.format(key, e[key]))
+        if as_string:
+            lines.append('{}: \'{}\'\n'.format(new_key or key, e[key]))
+        else:
+            lines.append('{}: {}\n'.format(new_key or key, e[key]))
+
 
 for e in d.entries:
     if e.guid == '578d8d3bf5e231fc9f850e55:578d8eaebe65944d662e0687:5b0595392b6a287c58f9f70d':
@@ -30,9 +34,32 @@ for e in d.entries:
     with open(root_dir + "/_posts/"+fname, "r") as infile:
         lines = infile.readlines()
         new_lines = lines[:2]
+        print(e.media_content)
         copy_value(e, new_lines, "guid")
+        copy_value(e, new_lines, "published", "rss_date")
         copy_value(e, new_lines, "itunes_season")
+        copy_value(e, new_lines, "itunes_episode")
+        copy_value(e, new_lines, "itunes_author")
+        copy_value(e, new_lines, "itunes_summary")
+        copy_value(e, new_lines, "itunes_duration", "itunes_duration", True)
+        copy_value(e, new_lines, "itunes_image")
+        copy_value(e, new_lines, "itunes_title", "itunes_title", True)
+        copy_value(e, new_lines, "itunes_episodetype")
+        
+        # 'links'
+        # 'link'
+        # 'id'
+        # 'guidislink'
+        # 'summary'
+        # 'summary_detail'
+        # 'content'
+        # 'image'
+        # 'media_content'
+        
+        
+        
         new_lines = new_lines + lines[2:]
     with open(root_dir + "/_posts/"+fname, "w") as outfile:
-        outfile.write(''.join(new_lines))
+        print()
+        # outfile.write(''.join(new_lines))
 
